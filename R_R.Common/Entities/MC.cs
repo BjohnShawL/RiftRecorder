60e69f48f4b6a4ddb8ca09b066973f22f2bd6714
Ben Liddle <porkchopdt@gmail.com>
2019-08-22 22:18:18 +0100

    finished entities

--- R_R.Common/Entities/MC.cs
+++ R_R.Common/Entities/MC.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace R_R.Common.Entities
 {
     public class MC
     {
-        
+        [Key]
+        public int id { get; set; }
+        public R_RUser User { get; set; }
+        public int UserId { get; set; }
+        public List<Game> Games { get; set; }
+
     }
 }
--- R_R.Common/Entities/Status.cs
+++ R_R.Common/Entities/Status.cs
@@ -8,6 +8,11 @@ namespace R_R.Common.Entities
 {
     public class Status
     {
-        
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public int Effect { get; set; }
+        public bool IsPositive { get; set; }
+        public Character Character { get; set; }
+        public int CharacterId { get; set; }
     }
 }
